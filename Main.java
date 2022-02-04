00b92d08b2cc24bf3b9ef33e2b85b1ee5ac49331
RedBert-X <ab.meyrumyan1>
2022-02-04 14:38:11 +0400

    Add Main

--- Main.java
+++ Main.java
@@ -1,7 +1,64 @@
 package com.aca;
 
+
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(10 / 2);
+        Scanner scanner = new Scanner(System.in);
+        /*
+        System.out.println("Input min to convert in hour.");
+        int min = scanner.nextInt();
+        int hour = min / 60;
+        int rm = min % 60;
+        System.out.println(hour + ":" + rm);
+         */
+        //int third = 1 + 3 + 5 + 7;
+/*
+        System.out.println("write three numbers");
+        int first = scanner.nextInt();
+        int second = scanner.nextInt();
+        int third = scanner.nextInt();
+
+        int sum = first + second + third;
+        int avg = sum / 3;
+
+        System.out.println("answer is:" + sum);
+        System.out.println("answer is:" + avg);
+ */
+        /*
+        System.out.println("enter value");
+        int num = scanner.nextInt();
+
+        int lastDigit = num % 10;
+
+        System.out.println("last digit of num: " + lastDigit);
+         */
+        /*
+        Random random = new Random();
+
+        int rand = random.nextInt(30) + 15;
+        */
+
+/*
+        int num = scanner.nextInt();
+
+        boolean isOdd = num % 2 == 1;
+
+        String str = (isOdd ? "Odd" :"Not odd");
+        System.out.println(str);
+ */
+
+
+
+
+
+
+
+
+
+
+
     }
 }
